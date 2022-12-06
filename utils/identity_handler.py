bfb117f957842cfaee6fe64ea0eed52e7e82b51f
Aaron <aaron@lastoryx.com>
2022-12-06 02:03:51 -0800

    Reboot functionality.

--- utils/identity_handler.py
+++ utils/identity_handler.py
@@ -12,8 +12,8 @@ async def reboot_bots(ctx, bot):
     if authorize_user(ctx.message.author):
         print(f"{ctx.message.author} rebooted {bot}.")
         await ctx.send(f" :computer:  {bot} is rebooting.")
-        os.execv(sys.executable, ["git pull"])
-        os.execv(sys.executable, ["python3", sys.argv])
+        os.system("git pull")
+        os.system(f"python3 {sys.argv}")
     else:
         await ctx.send(
             f" :computer:  Only admins can reboot {bot}!")
