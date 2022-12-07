976d551d510e082d33834e1dbe7af957e700c22f
Aaron <aaron@lastoryx.com>
2022-12-07 11:21:47 -0800

    Updated command list.

--- setup/command_list.py
+++ setup/command_list.py
@@ -8,17 +8,17 @@ def command_list(ctx, admin, bot_name):
                     f"List commands with `{bot_name} commands`\n\n" \
                     f"**Conversational Triggers**:\n" \
                     f"The bot can respond to some natural speech. Here are some examples:\n\n" \
-                    f">Hi / Hello / Hey {bot_name.capitalize()}.\n" \
-                    f">What's up, {bot_name.capitalize()}?\n" \
-                    f">How\'s your day going, {bot_name.capitalize()}.\n" \
-                    f">Thanks {bot_name.capitalize()}!\n" \
+                    f"> Hi / Hello / Hey {bot_name.capitalize()}.\n" \
+                    f"> What's up, {bot_name.capitalize()}?\n" \
+                    f"> How\'s your day going, {bot_name.capitalize()}.\n" \
+                    f"> Thanks {bot_name.capitalize()}!\n" \
                     f"\t\tSorry {bot_name.capitalize()}!\n\n" \
                     f"**Stoplight Commands**:\n" \
-                    f">Let someone know that you\'re good-to-go with `{bot_name} green`\n" \
-                    f">If the conversation should proceed with caution, use `{bot_name} yellow`\n" \
-                    f">If you need to stop the conversation and ping an admin, use `{bot_name} red`"
+                    f"> Let someone know that you\'re good-to-go with `{bot_name} green`\n" \
+                    f"> If the conversation should proceed with caution, use `{bot_name} yellow`\n" \
+                    f"> If you need to stop the conversation and ping an admin, use `{bot_name} red`"
     admin_commands = f"\n\n**ADMIN Commands**:\n" \
-                     f">Reboot bot with `{bot_name} reboot`\n\n"
+                     f"> Reboot bot with `{bot_name} reboot`\n\n"
     tail = f"🟢\t 🟡\t🔴\n"
 
     if admin:
