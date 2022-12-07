2a8e836545a0387a7a81f80f9020589fce35ea8d
Aaron <aaron@lastoryx.com>
2022-12-07 11:23:22 -0800

    More formatting fixes.

--- setup/command_list.py
+++ setup/command_list.py
@@ -13,12 +13,12 @@ def command_list(ctx, admin, bot_name):
                     f"> \t\tHow\'s your day going, {bot_name.capitalize()}.\n" \
                     f"> \t\tThanks {bot_name.capitalize()}!\n" \
                     f"> \t\tSorry {bot_name.capitalize()}!\n\n" \
-                    f"**Stoplight Commands**:\n" \
+                    f"**Stoplight Commands**:\n\n" \
                     f"> \t\tLet someone know that you\'re good-to-go with `{bot_name} green`\n" \
                     f"> \t\tIf the conversation should proceed with caution, use `{bot_name} yellow`\n" \
                     f"> \t\tIf you need to stop the conversation and ping an admin, use `{bot_name} red`"
-    admin_commands = f"\n\n**ADMIN Commands**:\n" \
-                     f"> Reboot bot with `{bot_name} reboot`\n\n"
+    admin_commands = f"\n\n**ADMIN Commands**:\n\n" \
+                     f"> \t\tReboot bot with `{bot_name} reboot`\n\n"
     tail = f"🟢\t 🟡\t🔴\n"
 
     if admin:
