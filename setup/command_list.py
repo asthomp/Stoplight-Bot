bb795d5b62ffcec5fba316f91ce4509bc3eecfb5
Aaron <aaron@lastoryx.com>
2022-12-07 11:17:49 -0800

    Updated command list.

--- setup/command_list.py
+++ setup/command_list.py
@@ -2,23 +2,23 @@
 # Customize using f-strings.
 def command_list(ctx, admin, bot_name):
     user_commands = f"\n\t🟢\t 🟡\t🔴\n" \
-                    f"==STOPLIGHT-BOT COMMANDS ==\n\n" \
+                    f"==STOPLIGHT-BOT COMMANDS ==\n" \
                     f"Most commands do not work in private messages.\n\n" \
                     f"Ping bots with `{bot_name} ping`\n" \
                     f"List commands with `{bot_name} commands`\n\n" \
                     f"**Conversational Triggers**:\n" \
-                    f"The bot can respond to some natural speech. Here's some examples:\n" \
+                    f"The bot can respond to some natural speech. Here are some examples:\n" \
                     f">Hi / Hello / Hey {bot_name.capitalize()}.\n" \
-                    f">What's up, {bot_name.capitalize()}.\n" \
+                    f">What's up, {bot_name.capitalize()}?\n" \
                     f">How\'s your day going, {bot_name.capitalize()}.\n" \
                     f">Thanks {bot_name.capitalize()}!\n" \
                     f"\t\tSorry {bot_name.capitalize()}!\n\n" \
                     f"**Stoplight Commands**:\n" \
-                    f"Let someone know that you\'re good-to-go with `{bot_name} green`\n" \
-                    f"If the conversation should proceed with caution, use `{bot_name} yellow`\n" \
-                    f"If you need to stop the conversation and ping an admin, use `{bot_name} red`\n"
+                    f">Let someone know that you\'re good-to-go with `{bot_name} green`\n" \
+                    f">If the conversation should proceed with caution, use `{bot_name} yellow`\n" \
+                    f">If you need to stop the conversation and ping an admin, use `{bot_name} red`\n"
     admin_commands = f"\n\n**ADMIN Commands**:\n" \
-                     f"Reboot bot with `{bot_name} reboot`\n"
+                     f">Reboot bot with `{bot_name} reboot`\n"
 
 
 if admin:
