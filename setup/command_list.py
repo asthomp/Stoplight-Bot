86ec1a5e048b975753f8344b65a6401846f2218e
Aaron <aaron@lastoryx.com>
2022-12-07 11:24:38 -0800

    More formatting fixes.

--- setup/command_list.py
+++ setup/command_list.py
@@ -4,8 +4,8 @@ def command_list(ctx, admin, bot_name):
     user_commands = f"\n\t🟢\t 🟡\t🔴\n\n" \
                     f"==STOPLIGHT-BOT COMMANDS ==\n" \
                     f"Most commands do not work in private messages.\n\n" \
-                    f"Ping bots with `{bot_name} ping`\n" \
-                    f"List commands with `{bot_name} commands`\n\n" \
+                    f"> \t\tPing bots with `{bot_name} ping`\n" \
+                    f"> \t\tList commands with `{bot_name} commands`\n\n" \
                     f"**Conversational Triggers**:\n" \
                     f"The bot can respond to some natural speech. Here are some examples:\n\n" \
                     f"> \t\tHi / Hello / Hey {bot_name.capitalize()}.\n" \
@@ -14,9 +14,9 @@ def command_list(ctx, admin, bot_name):
                     f"> \t\tThanks {bot_name.capitalize()}!\n" \
                     f"> \t\tSorry {bot_name.capitalize()}!\n\n" \
                     f"**Stoplight Commands**:\n\n" \
-                    f"> \t\tLet someone know that you\'re good-to-go with `{bot_name} green`\n" \
-                    f"> \t\tIf the conversation should proceed with caution, use `{bot_name} yellow`\n" \
-                    f"> \t\tIf you need to stop the conversation and ping an admin, use `{bot_name} red`"
+                    f"> \t\tIf you\'re good-to-go, use `{bot_name} green`\n" \
+                    f"> \t\tIf everyone should proceed with caution, use `{bot_name} yellow`\n" \
+                    f"> \t\tIf the conversation should stop, use `{bot_name} red`"
     admin_commands = f"\n\n**ADMIN Commands**:\n\n" \
                      f"> \t\tReboot bot with `{bot_name} reboot`\n\n"
     tail = f"🟢\t 🟡\t🔴\n"
