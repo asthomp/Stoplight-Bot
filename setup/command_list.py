b090668e6208db88c8b251c0a9ddef4f9c7083f3
Aaron <aaron@lastoryx.com>
2022-12-07 11:06:16 -0800

    Updated command list.

--- setup/command_list.py
+++ setup/command_list.py
@@ -7,11 +7,12 @@ def command_list(ctx, admin, bot_name):
                     f"Ping bots with `{bot_name} ping`\n" \
                     f"List commands with `{bot_name} commands`\n\n" \
                     f"**Conversation Triggers**:\n" \
+                    f"These are just samples - the bot may respond to other conversational triggers.\n\n" \
                     f"Hi / Hello / Hey {bot_name.capitalize()}.\n" \
-                    f"What's up / What are you doing {bot_name.capitalize()}.\n" \
-                    f"How are you/How\'s your day going {bot_name.capitalize()}.\n" \
-                    f"Thanks / Thank you {bot_name.capitalize()}.\n" \
-                    f"Sorry / My bad {bot_name.capitalize()}. \n\n" \
+                    f"What's up, {bot_name.capitalize()}.\n" \
+                    f"How\'s your day going, {bot_name.capitalize()}.\n" \
+                    f"Thanks {bot_name.capitalize()}!\n" \
+                    f"Sorry {bot_name.capitalize()}!\n\n" \
                     f"**Stoplight Triggers**:\n" \
                     f"Let someone know that you\'re good-to-go with `{bot_name} green`\n" \
                     f"If the conversation should proceed with caution use `{bot_name} yellow`\n" \
