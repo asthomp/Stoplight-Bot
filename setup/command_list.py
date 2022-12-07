4034d93d95c86d8dc12ab2c0d80293938d815ad6
Aaron <aaron@lastoryx.com>
2022-12-07 10:56:52 -0800

    Command list formatting.

--- setup/command_list.py
+++ setup/command_list.py
@@ -13,9 +13,9 @@ def command_list(ctx, admin, bot_name):
                     f"Thanks / Thank you, {bot_name.capitalize()}.\n" \
                     f"Sorry / My bad {bot_name.capitalize()}. \n\n" \
                     f"**Stoplight Triggers**:\n" \
-                    f"Let someone know that you\'re good-to-go with `{bot_name} green'\n" \
-                    f"If the conversation should proceed with caution use `{bot_name} yellow'\n" \
-                    f"To stop the conversation and ping an admin use `{bot_name} red'\n"
+                    f"Let someone know that you\'re good-to-go with `{bot_name} green`\n" \
+                    f"If the conversation should proceed with caution use `{bot_name} yellow`\n" \
+                    f"To stop the conversation and ping an admin use `{bot_name} red`\n"
     admin_commands = f"\n\n**ADMIN Commands**:\n" \
                      f"Reboot bot with `{bot_name} reboot`\n"
     if admin:
