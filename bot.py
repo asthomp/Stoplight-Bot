9c9284141b95821fcf16e884f1a6c8eaa10f6f70
Aaron <aaron@lastoryx.com>
2022-12-11 22:15:57 -0800

    Fixing intents.

--- bot.py
+++ bot.py
@@ -23,7 +23,6 @@ class Bot(commands.Bot):
         intents.members = True
         intents.presences = True
         intents.messages = True
-        intents.message_content = True
 
         super().__init__(command_prefix=prefixes, intents=intents)
 
